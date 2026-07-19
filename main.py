64682576407c51a50ebd9fabe9f0f07ef7b8a56c
NoOneHardy <silas.hardegger@outlook.com>
2026-07-19 23:43:46 +0200

    simplify example

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import cv2
-import matplotlib.pyplot as plt
 import mediapipe as mp
+from mediapipe import ImageFormat
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
 import lmonimage
 
-base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
-options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
+base_options: python.BaseOptions = python.BaseOptions(model_asset_path='hand_landmarker.task')
+options: vision.HandLandmarkerOptions = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
 
-detector = vision.HandLandmarker.create_from_options(options)
+detector: vision.HandLandmarker = vision.HandLandmarker.create_from_options(options)
+capture: cv2.VideoCapture = cv2.VideoCapture(0)
 
-image = mp.Image.create_from_file('image.jpg')
+while capture.isOpened():
+    frame: cv2.Mat
+    success, frame = capture.read()
+    if not success:
+        print("Ignoring empty camera frame.")
+        continue
+    frame = cv2.flip(frame, 1)
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    media_image = mp.Image(ImageFormat.SRGB, image)
+    detected_image = lmonimage.draw_landmarks_on_image(image, detector.detect(media_image))
+    image = cv2.cvtColor(detected_image, cv2.COLOR_RGB2BGR)
+    cv2.imshow('image', image)
 
-detection = detector.detect(image)
-
-annotated_image = lmonimage.draw_landmarks_on_image(image.numpy_view(), detection)
-plt.imshow(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2RGBA))
-plt.axis('off')
-plt.show()
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
---
+++ test.py
@@ -0,0 +1,51 @@
+import cv2
+import mediapipe as mp
+from mediapipe.tasks.python.vision import drawing_utils
+from mediapipe.tasks.python.vision.core.vision_task_running_mode import VisionTaskRunningMode
+from mediapipe.tasks.python.vision.hand_landmarker import HandLandmarker, HandLandmarkerOptions, HandLandmarkerResult, \
+    HandLandmarksConnections
+
+cap: cv2.VideoCapture = cv2.VideoCapture(0)
+mp_drawing = drawing_utils
+
+def handle_image(result: HandLandmarkerResult, image: mp.Image, _):
+    bgr_image = cv2.cvtColor(image.numpy_view(), cv2.COLOR_RGB2BGR)
+    for hand in result.hand_landmarks:
+        mp_drawing.draw_landmarks(bgr_image, hand, [
+            HandLandmarksConnections.Connection(4, 8),
+            HandLandmarksConnections.Connection(8, 12),
+            HandLandmarksConnections.Connection(12, 16),
+            HandLandmarksConnections.Connection(16, 20)
+        ])
+
+    rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
+    cv2.imshow('Result', rgb_image)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+lm_options = HandLandmarkerOptions(
+    base_options=mp.tasks.BaseOptions(model_asset_path='hand_landmarker.task'),
+    num_hands=1,
+    running_mode=VisionTaskRunningMode.LIVE_STREAM,
+    result_callback=handle_image)
+lm = HandLandmarker.create_from_options(lm_options)
+
+while cap.isOpened():
+    frame: cv2.Mat
+    isSuccess, frame = cap.read()
+    if not isSuccess:
+        print('Ignoring empty camera frame.')
+        continue
+
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+    try:
+        lm.detect_async(image=mp_image, timestamp_ms=int(cap.get(cv2.CAP_PROP_POS_MSEC)))
+    except ValueError as e:
+        if 'timestamp_ms' in str(e):
+            pass
+
+
+
